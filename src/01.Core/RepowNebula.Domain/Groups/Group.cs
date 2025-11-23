a2b74180d8d20f8266ce66db748f1d13fc1ef145
Mostafa Hasanpour <Mostafa.hasanpuor@gmail.com>
2025-11-23 22:35:41 +0330

    Start Implementing Core Domains

---
+++ src/01.Core/RepowNebula.Domain/Groups/Group.cs
@@ -0,0 +1,14 @@
+﻿namespace RepowNebula.Domain.Groups;
+
+public class RepositoryGroup
+{
+    public string Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string ParentGroupId { get; private set; }
+
+    public RepositoryGroup ParentGroup { get; private set; }
+
+    public string Url { get; private set; }
+}
---
+++ src/01.Core/RepowNebula.Domain/Repositories/Repository.cs
@@ -0,0 +1,43 @@
+﻿using RepowNebula.Domain.Groups;
+
+namespace RepowNebula.Domain.Repositories;
+
+public class Repository
+{
+    public Repository(string id, string name, string groupId, RepositoryGroup group, string url)
+    {
+        Id = id;
+        Name = name;
+        GroupId = groupId;
+        Group = group;
+        Url = url;
+    }
+
+    public string Id { get; private set; }
+
+    public string Name { get; private set; }
+
+    public string GroupId { get; private set; }
+
+    public RepositoryGroup Group { get; private set; }
+
+    public string Url { get; private set; }
+
+    public static Repository Create(string id, string name, RepositoryGroup group, string url)
+    {
+        if (id is null) throw new ArgumentNullException(nameof(id));
+        if (name is null) throw new ArgumentNullException(nameof(name));
+        if (group is null) throw new ArgumentNullException(nameof(group));
+        if (url is null) throw new ArgumentNullException(nameof(url));
+
+        id = id.Trim();
+        name = name.Trim();
+        url = url.Trim();
+
+        if (id.Length == 0) throw new ArgumentException("Id cannot be empty or whitespace.", nameof(id));
+        if (name.Length == 0) throw new ArgumentException("Name cannot be empty or whitespace.", nameof(name));
+        if (url.Length == 0) throw new ArgumentException("Url cannot be empty or whitespace.", nameof(url));
+
+        return new Repository(id, name, group.Id, group, url);
+    }
+}
